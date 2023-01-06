5bd0e407ad637d72830f1524083f8ef022e36845
ArcyHeytr <115278726+Arcyhetyr@users.noreply.github.com>
2023-01-06 16:14:26 +0100

    initial

---
+++ Library/Library.ConsoleApp/BooksService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Library.Domain;
+using Library.Persistence;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.ConsoleApp
+{
+    class BooksService
+    {
+
+        public void AddBook()
+        {
+            Console.WriteLine("Enter book title:");
+            string title = Console.ReadLine();
+            Console.WriteLine("Enter book author:");
+            string author = Console.ReadLine();
+            Console.WriteLine("Enter book publication year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter book ISBN:");
+            string isbn = Console.ReadLine();
+            Console.WriteLine("Enter book stock:");
+            int stock = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter book price:");
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+
+
+            Book booktoAdd = new Book(title, author, year, isbn, stock, price);
+            //tu bedzie dodawanie
+        }
+
+        public void RemoveBook()
+        {
+            Console.WriteLine("Enter book title to remove:");
+            string title = Console.ReadLine();
+            //tutaj bedzie usuwanie
+
+
+        }
+
+        public void ListBooks()
+        {
+            Console.WriteLine("Here is the list of books:");
+            //tu bedzie  listowanie
+
+        }
+        public void ChangeState()
+        {
+            Console.WriteLine("Enter book title to change stock:");
+            string title = Console.ReadLine();
+            Convert.ToInt32(title);
+            Console.WriteLine("State of title has ben changed and it is: " + title);
+            //tu bedzie zmiana
+        }
+        
+    }
+}
---
+++ Library/Library.ConsoleApp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Library.Domain;
+using Library.Persistence;
+
+namespace Library.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Ślepnąc od świateł", "Jakub Żulczyk", 2015, "ISBN", 10, 19.99m);
+            BooksRepository repository = new BooksRepository();
+            while (true)
+            {
+                Console.WriteLine("Type your Login:");
+                string login = Console.ReadLine();
+                Console.WriteLine("Type your password");
+                string password = Console.ReadLine();
+                if (login != "Admin" && password != "password")
+                {
+                    Console.WriteLine("Access Denied");
+
+                }
+                else
+                {
+                    Console.WriteLine("Access Granted");
+                
+                string command;
+                    BooksService Boook = new BooksService();
+                    while (true)
+                    {
+                        // Console.Clear();
+                        Console.WriteLine("Add - add a book");
+                        Console.WriteLine("Del - delete a book");
+                        Console.WriteLine("View - Display all books");
+                        Console.WriteLine("Change - change the stock of a book");
+                        Console.WriteLine("Add an order - add a new order");
+                        Console.WriteLine("Order list - display all orders");
+                        Console.WriteLine("Exit - exit the program");
+                        Console.WriteLine();
+                        Console.WriteLine("Type your command");
+                        command = Console.ReadLine();
+                        if (command == "exit")
+                        {
+                            Console.WriteLine("Im leaving, bye!");
+                            break;
+                        }
+                        string continue_while;
+                        Console.WriteLine("Do you want to exit menu?(yes/no)");
+                        continue_while = Console.ReadLine();
+                        if (continue_while == "yes")
+                        {
+                            Console.WriteLine("Im leaving, bye!");
+                            break;
+                        }
+
+                        else
+                        {
+
+                            if (command == "add")
+                            {
+                                Console.WriteLine("Attempt to add a book");
+                                Boook.AddBook();
+
+
+
+
+                            }
+                            if (command == "del")
+                            {
+                                Console.WriteLine("Attempt to delete a book");
+                                Boook.RemoveBook();
+                            }
+                            if (command == "view")
+                            {
+                                Console.WriteLine("Attemt to Display all books");
+                                Boook.ListBooks();
+                            }
+                            if (command == "change")
+                            {
+                                Console.WriteLine("Attempt to change the stock: ");
+                                Boook.ChangeState();
+                            }
+                            if (command == "add an order")
+                            {
+                                Console.WriteLine("Attemt to add an order");
+                            }
+                            if (command == "order list")
+                            {
+                                Console.WriteLine("Attemt to display all orders");
+                            }
+
+                            else break;
+                        }
+                    }
+
+
+                }break;
+
+
+            }
+
+        }
+    }
+}
